1bf753b2ca1e1a6beea1fead2f697090bbc41d48
Eeva-Jonna Panula <eevajonna.panula@gmail.com>
2023-12-30 07:22:17 +0200

    Add ability to select items for summary

--- app/src/main/java/com/eevajonna/bragdocument/ui/components/GenerateSummaryDialog.kt
+++ app/src/main/java/com/eevajonna/bragdocument/ui/components/GenerateSummaryDialog.kt
@@ -7,9 +7,11 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.selection.selectable
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
@@ -27,14 +29,19 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogProperties
 import com.eevajonna.bragdocument.R
 import com.eevajonna.bragdocument.data.BragItem
+import com.eevajonna.bragdocument.ui.theme.BragDocumentTheme
 import java.time.LocalDate
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -57,6 +64,18 @@ fun GenerateSummaryDialog(
         mutableStateOf(initialTitle)
     }
 
+    var selectedItems by remember {
+        mutableStateOf(itemsToSelect.map { it.id }.toSet())
+    }
+
+    fun toggleSelect(item: BragItem) {
+        if (selectedItems.contains(item.id)) {
+            selectedItems = selectedItems.minus(item.id)
+        } else {
+            selectedItems = selectedItems.plus(item.id)
+        }
+    }
+
     Dialog(onDismissRequest = { onDismissRequest() }, properties = DialogProperties(usePlatformDefaultWidth = false)) {
         Scaffold(
             topBar = {
@@ -74,9 +93,10 @@ fun GenerateSummaryDialog(
                     },
                     actions = {
                         TextButton(
-                            enabled = title.isNotEmpty() && newSummary.isNullOrEmpty() && loading.not(),
+                            enabled = title.isNotEmpty() && newSummary.isNullOrEmpty() && loading.not() && selectedItems.count() >= 3,
                             onClick = {
-                                onAddItem(title, itemsToSelect)
+                                val items = itemsToSelect.filter { selectedItems.contains(it.id) }
+                                onAddItem(title, items)
                             },
                         ) {
                             Text(stringResource(R.string.button_generate))
@@ -102,9 +122,13 @@ fun GenerateSummaryDialog(
                 if (error.isNotEmpty()) {
                     Row() {
                         Text(error)
-                        Button(onClick = {
-                            onAddItem(title, itemsToSelect)
-                        }) {
+                        Button(
+                            enabled = selectedItems.count() >= 3,
+                            onClick = {
+                                val items = itemsToSelect.filter { selectedItems.contains(it.id) }
+                                onAddItem(title, items)
+                            },
+                        ) {
                             Text(text = stringResource(R.string.button_try_again))
                         }
                     }
@@ -117,23 +141,77 @@ fun GenerateSummaryDialog(
                 if (newSummary.isNullOrEmpty().not()) Text(summaryTitleText, style = MaterialTheme.typography.titleLarge)
                 if (loading) CircularProgressIndicator()
                 Text("$newSummary")
-                if (itemsToSelect.isNotEmpty()) Text(stringResource(R.string.items_included), style = MaterialTheme.typography.titleLarge)
-                itemsToSelect.map {
+                if (newSummary.isNullOrEmpty()) {
                     Text(
-                        it.text,
-                        color = MaterialTheme.colorScheme.onSecondaryContainer,
-                        style = MaterialTheme.typography.titleMedium,
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .clip(
-                                GenerateSummaryDialog.shape,
-                            )
-                            .background(MaterialTheme.colorScheme.secondaryContainer)
-                            .padding(GenerateSummaryDialog.padding),
+                        stringResource(R.string.items_included),
+                        style = MaterialTheme.typography.titleLarge,
                     )
+                    Text(stringResource(R.string.select_at_least_three_items))
+
+                    itemsToSelect.map {
+                        SelectableItem(it, selectedItems.contains(it.id)) {
+                            toggleSelect(it)
+                        }
+                    }
                 }
             }
         }
+    }
+}
+
+@Composable
+fun SelectableItem(item: BragItem, selected: Boolean, toggleSelect: (BragItem) -> Unit) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clip(
+                GenerateSummaryDialog.shape,
+            )
+            .selectable(
+                selected = selected,
+                role = Role.Checkbox,
+            ) {
+                toggleSelect(item)
+            }
+            .background(MaterialTheme.colorScheme.secondaryContainer)
+            .padding(GenerateSummaryDialog.padding),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Icon(
+            imageVector = Icons.Default.Check,
+            contentDescription = null,
+            tint = if (selected) MaterialTheme.colorScheme.onSecondaryContainer else Color.Transparent,
+            modifier = Modifier.weight(1f),
+        )
+        Text(
+            text = item.text,
+            color = MaterialTheme.colorScheme.onSecondaryContainer,
+            style = MaterialTheme.typography.titleMedium,
+            modifier = Modifier.weight(5f),
+        )
+    }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun GenerateSummaryDialogPreview() {
+    fun a(b: String, c: List<BragItem>) {}
+    BragDocumentTheme {
+        GenerateSummaryDialog(
+            itemsToSelect = listOf(
+                BragItem(0, LocalDate.now(), "Helped my colleague to solve an issue.", 1),
+                BragItem(1, LocalDate.now().minusMonths(1), "Mentored a more junior colleague", 2),
+                BragItem(2, LocalDate.now().minusMonths(4), "Solved very complex issues"),
+                BragItem(1, LocalDate.now().minusYears(1), "Mentored a more junior colleague", 2),
+                BragItem(2, LocalDate.now().minusMonths(6).minusYears(1), "Solved very complex issues"),
+            ),
+            newSummary = "",
+            error = "",
+            loading = false,
+            onDismissRequest = {},
+            onAddItem = ::a,
+        )
     }
 }
 
