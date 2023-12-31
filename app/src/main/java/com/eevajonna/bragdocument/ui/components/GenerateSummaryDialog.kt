8b04eb56015ecd3da035b03def4be9c03149714f
Eeva-Jonna Panula <eevajonna.panula@gmail.com>
2023-12-31 07:24:19 +0200

    Refactor loading spinner

--- app/src/main/java/com/eevajonna/bragdocument/ui/components/GenerateSummaryDialog.kt
+++ app/src/main/java/com/eevajonna/bragdocument/ui/components/GenerateSummaryDialog.kt
@@ -2,10 +2,13 @@ package com.eevajonna.bragdocument.ui.components
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
@@ -27,8 +30,10 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -123,7 +128,14 @@ fun GenerateSummaryDialog(
                     }
                 }
                 when {
-                    loading -> CircularProgressIndicator()
+                    loading -> Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                        CircularProgressIndicator(
+                            strokeWidth = 12.dp,
+                            strokeCap = StrokeCap.Round,
+                            color = MaterialTheme.colorScheme.secondary,
+                            modifier = Modifier.size(120.dp),
+                        )
+                    }
                     newSummary.isNullOrEmpty().not() -> {
                         GeneratedSummary(summaryTitle = title, summaryText = newSummary)
                     }
